8b815434eaab6fd9b42b8e2383ea1fe8155ab3d0
Asko Nõmm <asko@bien.ee>
2021-07-24 19:40:02 -0300

    Make it so that Lein also has all dependencies

--- project.clj
+++ project.clj
@@ -1,12 +1,15 @@
-(defproject org.clojars.askonomm/blocko "0.1"
+(defproject org.clojars.askonomm/blocko "0.2"
   :description "A block-based WYSIWYG editor"
   :url "https://github.com/askonomm/blocko"
   :min-lein-version "2.0.0"
-  :dependencies [[reagent "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/clojurescript "1.10.866"]
+                 [cljsjs/react "17.0.2-0"]
+                 [cljsjs/react-dom "17.0.2-0"]
+                 [reagent "1.1.0"]
                  [re-frame "1.2.0"]]
   :plugins [[camechis/deploy-uberjar "0.3.0"]]
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :repositories [["releases" {:url "https://clojars.org"
-                              :sign-releases false}]]
+  :source-paths ["src"]
   :uberjar-name "blocko.jar")
