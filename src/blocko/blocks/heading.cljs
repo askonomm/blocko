c357dc2efec18bf69fc4f3eabd997e420524464e
Asko Nõmm <asko@bien.ee>
2021-07-28 14:47:43 -0300

    Closes #19

--- src/blocko/blocks/heading.cljs
+++ src/blocko/blocks/heading.cljs
@@ -6,31 +6,66 @@
 
 (def focus-el-selector "textarea")
 
-(defn on-key-press [id event]
-  (when (or (= "Enter" (.-key event))
-            (= 13 (.-keyCode event)))
-    (.preventDefault event)
-    (let [new-block-id (str (random-uuid))]
-      (dispatch
-       [:add-block
-        {:position {:id id
-                    :insert :after}
-         :block {:id new-block-id
-                 :type "paragraph"
-                 :content ""}}])
-      (dispatch
-       [:set-focus
-        {:id new-block-id
-         :where :beginning}]))))
+(defn create-block!
+  "Creates a new block and focuses the cursor in it."
+  [id event]
+  (.preventDefault event)
+  (let [new-block-id (str (random-uuid))]
+    (dispatch [:add-block
+               {:position {:id id
+                           :insert :after}
+                :block {:id new-block-id
+                        :type "paragraph"
+                        :content ""}}])
+    (dispatch [:set-focus
+               {:id new-block-id
+                :where :beginning}])))
 
-(defn on-input [id height event]
+(defn delete-block!
+  "Checks if the content is empty and if it is, byebye block.
+  It does dispatch a special event however, which tries to 
+  find a block before this one - and then focus in it, for that smooth
+  user experience sweetness."
+  [id content event]
+  (when (empty? content)
+    (.preventDefault event)
+    (dispatch [:delete-block-and-focus-on-previous id])))
+
+(defn on-key-down!
+  "Use case 1: 
+  
+  Detect if the user pressed the `enter` key or not. If
+  the user did, we want to disable the default behaviour, which
+  usually is creating a new line, and instead create a new 
+  paragraph block below it.
+  
+  Use case 2:
+  
+  Detect if the user pressed the `backspace` key. if the the 
+  user did, and if there is no more `content`, delete the block."
+  [id event]
+  (cond (or (= "Enter" (.-key event))
+            (= 13 (.-keyCode event)))
+        (create-block! id event)
+
+        (or (= "Backspace" (.-key event))
+            (= 8 (.-keyCode event)))
+        (delete-block! id (.-value (.-target event)) event)))
+
+(defn on-input!
+  "Trigged during typing, updates the content of the block as well
+  as the height of the textarea."
+  [id height event]
   (dispatch
    [:update-heading-block
     {:id id
      :content (.-value (.-target event))}])
   (reset! height (.-scrollHeight (.-target event))))
 
-(defn block [id block]
+(defn block
+  "Renders the DOM output of the paragraph block and hooks to it
+  many of its necessary events."
+  [id block]
   (let [height (r/atom 30)]
     (fn []
       [:textarea
@@ -38,6 +73,6 @@
         :ref (fn [el] (when el (reset! height (.-scrollHeight el))))
         :default-value (get block :content)
         :placeholder "Start writing a heading ..."
-        :on-key-press #(on-key-press id %)
+        :on-key-down #(on-key-down! id %)
         :on-focus #(dispatch [:set-active-block id])
-        :on-input #(on-input id height %)}])))
+        :on-input #(on-input! id height %)}])))
